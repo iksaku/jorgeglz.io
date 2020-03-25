9d41d64002b840345dcb69d2e9143118ed17b173
Jorge González <iksaku@me.com>
2020-03-24 22:56:16 -0600

    Remove Dashboard and Auth routes from production (Not yet ready)

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-
-Auth::routes([
-    'confirm' => true,
-    'register' => false,
-    'reset' => true,
-    'verify' => true,
-]);
+if (!app()->environment('production')) {
+    Auth::routes([
+        'confirm' => true,
+        'register' => false,
+        'reset' => true,
+        'verify' => true,
+    ]);
+}
 
 Route::get('/', 'Blog\IndexController@index')->name('blog.index');
 Route::get('about', 'Blog\AboutController@index')->name('blog.about');
@@ -17,18 +16,20 @@ Route::prefix('posts')->group(function () {
     Route::get('{post}', 'Blog\PostController@show')->name('blog.post');
 });
 
-Route::prefix('dashboard')->name('dashboard.')->middleware(['auth'])->group(function () {
-    Route::get('/', 'Dashboard\IndexController@index')->name('index');
+if (!app()->environment('production')) {
+    Route::prefix('dashboard')->name('dashboard.')->middleware(['auth'])->group(function () {
+        Route::get('/', 'Dashboard\IndexController@index')->name('index');
 
-    Route::resource('posts', 'Dashboard\PostController');
-    Route::put('posts/{trashed_post}/restore', 'Dashboard\PostController@restore')
-        ->name('posts.restore');
+        Route::resource('posts', 'Dashboard\PostController');
+        Route::put('posts/{trashed_post}/restore', 'Dashboard\PostController@restore')
+            ->name('posts.restore');
 
-    Route::fallback(function () {
-        abort(404, 'Well, it looks like you reached nowhere...');
-    })->name('error')->where('any', '.*');
-});
+        Route::fallback(function () {
+            abort(404, 'Well, it looks like you reached nowhere...');
+        })->name('error')->where('any', '.*');
+    });
+}
 
 Route::fallback(function () {
     abort(404, 'Well, it looks like you reached nowhere...');
-})->name('blog.error')->where('any', '.*');
+})->name('blog.error');
