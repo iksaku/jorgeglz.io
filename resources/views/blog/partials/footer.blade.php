a13fcbc2e4c0100ae2385c103fc04be1784488d1
Jorge González <iksaku@me.com>
2020-03-29 23:20:32 -0600

    Fixed footer text-size

--- resources/views/blog/partials/footer.blade.php
+++ resources/views/blog/partials/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="z-40 bottom-0 inset-x-0 w-full bg-gray-200 border-t border-gray-300 shadow">
     <div class="container mx-auto px-4 flex items-center justify-between">
-        <section class="font-bold text-center py-4">
+        <section class="text-sm md:text-base font-bold py-4">
             &copy; {{ date('Y') }} Jorge González
         </section>
 
-        <ul class="flex text-2xl">
+        <ul class="flex text-xl md:text-2xl">
             @foreach(config('social') as $name => $data)
                 <li class="ml-4 sm:ml-8">
                     <a class="text-black"
