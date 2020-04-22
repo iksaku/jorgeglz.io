f2378a4c62be624b555f183a7840a36540733b7e
Jorge González <iksaku@me.com>
2020-04-22 01:06:53 -0500

    Fix potential XSS injection, default Heading IDs with prefix 'user-content'

--- config/markdown.php
+++ config/markdown.php
@@ -3,7 +3,6 @@
 return [
     'heading_permalink' => [
         'html_class' => 'permalink',
-        'id_prefix' => '',
         'insert' => 'after',
         'inner_contents' => '<span class="fas fa-link inline align-middle"></span>',
     ],
