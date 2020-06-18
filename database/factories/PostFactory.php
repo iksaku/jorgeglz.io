c7a40bf8f33cb64f5d6a94ee50abef252307c402
Jorge González <iksaku@me.com>
2020-06-17 21:41:51 -0500

    Updated packages, links now show different color properly

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -10,7 +10,6 @@ $factory->define(Post::class, function (Faker $faker) {
     return [
         'slug' => Str::slug($title = ucwords($faker->sentence)),
         'title' => $title,
-        'description' => $faker->sentences(3, true),
         'content' => $faker->paragraphs(6, true),
         'published_at' => $faker->dateTimeThisMonth,
     ];
