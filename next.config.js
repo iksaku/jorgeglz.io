9273bed87af36510bcd8f64912b4aa6b6288357d
Jorge González <yo@jorgeglz.io>
2022-01-10 07:19:10 -0600

    Extract MDX configuration from main Next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-const { createLoader } = require('simple-functional-loader')
-const path = require('path')
+const withMDX = require('./lib/mdx')
 
-module.exports = {
+module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
 
   images: {
@@ -11,76 +10,4 @@ module.exports = {
   redirects: async () => [
     { source: '/posts/:slug', destination: '/blog/:slug', permanent: true },
   ],
-
-  webpack: (config, options) => {
-    // MDX Configuration
-    const mdxExtension = /\.mdx?$/
-    const mdxConfig = [
-      options.defaultLoaders.babel,
-      {
-        loader: '@mdx-js/loader',
-        options: {
-          remarkPlugins: [
-            require('remark-gemoji'),
-            require('remark-external-links'),
-          ],
-          rehypePlugins: [
-            require('@mapbox/rehype-prism'),
-            require('rehype-slug'),
-            [
-              require('rehype-autolink-headings'),
-              {
-                behavior: 'wrap',
-              },
-            ],
-          ],
-        },
-      },
-    ]
-
-    const blogPath = path.resolve(process.cwd(), 'src/pages/blog')
-
-    // For non-blog mdx files, just keep it simple.
-    config.module.rules.push({
-      test: mdxExtension,
-      exclude: [blogPath],
-      use: mdxConfig,
-    })
-
-    // Load blog posts with Preview and Metadata
-    config.module.rules.push({
-      test: mdxExtension,
-      include: [blogPath],
-      oneOf: [
-        // Render Post Previews
-        {
-          resourceQuery: /preview/,
-          use: [
-            ...mdxConfig,
-            createLoader(function (src) {
-              const [preview] = src.split('<!--more-->')
-              return this.callback(null, preview)
-            }),
-          ],
-        },
-        // Full Post Rendering
-        {
-          use: [
-            ...mdxConfig,
-            createLoader(function (src) {
-              const content = [
-                'import Post from "@/components/blog/Post"',
-                'export default Post',
-                src,
-              ].join('\n')
-
-              return this.callback(null, content.replace('<!--more-->', ''))
-            }),
-          ],
-        },
-      ],
-    })
-
-    return config
-  },
-}
+})
