480216ada8c0037b94b0f235a45609620add2939
Jorge González <yo@jorgeglz.io>
2024-03-31 18:38:15 -0600

    Simplify blog post querying (+ transform)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,5 @@
-import type { CollectionEntry } from 'astro:content'
+import { getCollection, type CollectionEntry } from 'astro:content'
 import { DateTime } from 'luxon'
-
-export function toDateTime(date: Date, tz: string = 'America/Monterrey'): DateTime {
-  return DateTime.fromSQL(date.toISOString().split('T')[0]).setZone(tz)
-}
 
 type BlogEntry = CollectionEntry<'blog'>
 export type BlogPost = Omit<BlogEntry, 'data'> & {
@@ -12,19 +8,24 @@ export type BlogPost = Omit<BlogEntry, 'data'> & {
   }
 }
 
-export function transformBlogPost(post: CollectionEntry<'blog'>, baseUrl: URL): BlogPost {
-  return {
-    ...post,
-    data: {
-      ...post.data,
-      date: !post.data.date ? undefined : toDateTime(post.data.date),
-    },
-  }
-}
+export async function getBlogPosts(filter?: (entry: CollectionEntry<'blog'>) => boolean) {
+  return (await getCollection('blog', filter))
+    .map((post): BlogPost => {
+      const toDateTime = (date?: Date) =>
+        !date ? date : DateTime.fromJSDate(date, { zone: 'utc' }).setZone('America/Monterrey', { keepLocalTime: true })
 
-export function sortPostsByDate(postA: BlogPost, postB: BlogPost): number {
-  if (!postA.data.date) return -1
-  if (!postB.data.date) return 1
+      return {
+        ...post,
+        data: {
+          ...post.data,
+          date: toDateTime(post.data.date),
+        },
+      }
+    })
+    .sort((postA: BlogPost, postB: BlogPost) => {
+      if (!postA.data.date) return -1
+      if (!postA.data.date) return 1
 
-  return Math.sign(postB.data.date!.toSeconds() - postA.data.date!.toSeconds())
+      return Math.sign(postB.data.date!.toSeconds() - postA.data.date!.toSeconds())
+    })
 }
